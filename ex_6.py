aac1dce6e081dcc69991af810a0ed5cb3afc80d7
fernando brito <130664535+britoGEMIDA@users.noreply.github.com>
2024-08-09 08:43:56 -0300

    comit final

--- ex_6.py
+++
@@ -1,8 +0,0 @@
-# Frase original
-frase = "Quero ir para praia."
-
-# Substituição da palavra "praia" por "sítio"
-nova_frase = frase.replace("praia", "sítio")
-
-# Exibição da nova frase
-print(nova_frase)
